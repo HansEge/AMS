9e13d64e5747d6410a093b564887c10769abc899
Stinus Skovgaard <stinus2@gmail.com>
2018-05-29 11:07:29 +0200

    tilføjer button interrupt

--- Kode/GraficTFT_Test/GraficTFT_Test/Debug/TFTdriver.d
+++ Kode/GraficTFT_Test/GraficTFT_Test/Debug/TFTdriver.d
@@ -15,7 +15,9 @@ TFTdriver.d TFTdriver.o: .././TFTdriver.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- .././TFTdriver.h .././DotFactory.h
+ .././TFTdriver.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ .././DotFactory.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -50,5 +52,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
 .././TFTdriver.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
 .././DotFactory.h:
